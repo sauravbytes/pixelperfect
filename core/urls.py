506882cfc297729f7f6eb6e6cb94aa1307b88e1d
bhadrakalii <choudharysaurav214@gmail.com>
2025-04-02 16:57:24 +0545

    image storing in database

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from core import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='Homepage'),
-    path('services', views.ServiceView.as_view(), name='services')
+    path('services', views.ServiceView.as_view(), name='services'),
+    path('image-upload', views.upload_image, name='image-upload'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from django.views.generic import View
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
+from .models import ImageUpload
 
 # Create your views here.
 
@@ -14,4 +17,17 @@ class ServiceView(View):
 
     def get(self, request):
         return render(request, 'services.html')
+
+
+def upload_image(request):
+    if request.method == "POST":
+        if "image" not in request.FILES:
+            return HttpResponseBadRequest("No image uploaded.")
+
+        image = request.FILES["image"]
+        ImageUpload.objects.create(image=image)
+        return HttpResponse("Image uploaded successfully!")
+
+    return render(request, "index.html")
+
 
